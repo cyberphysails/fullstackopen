e7a7f8169cf89d3f7e9fb82b9d64d43dccb44b61
cyberphysails <physails@outlook.com>
2022-02-08 22:29:45 +0800

    add exercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -15,19 +15,11 @@ function Display(props) {
   )
 }
 
-function App() {
-  const [ good, setGood ] = useState(0);
-  const [ neutral, setNeutral ] = useState(0);
-  const [ bad, setBad ] = useState(0);
-
+function Statistics({good, neutral, bad}) {
   const all = bad + neutral + good;
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button onClick={()=>setGood(good + 1)} text="good"></Button>
-      <Button onClick={()=>setNeutral(neutral + 1)} text="neutral"></Button>
-      <Button onClick={()=>setBad(bad + 1)} text="bad"></Button>
       <h1>Statistics</h1>
       <Display text="good" count={good} />
       <Display text="neutral" count={neutral} />
@@ -35,6 +27,24 @@ function App() {
       <Display text="all" count={all} />
       <Display text="average" count={(good-bad)/all} />
       <Display text="positive" count={(100*good/all)+"%"} />
+    </div>
+  )
+}
+
+function App() {
+  const [ good, setGood ] = useState(0);
+  const [ neutral, setNeutral ] = useState(0);
+  const [ bad, setBad ] = useState(0);
+
+  
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={()=>setGood(good + 1)} text="good"></Button>
+      <Button onClick={()=>setNeutral(neutral + 1)} text="neutral"></Button>
+      <Button onClick={()=>setBad(bad + 1)} text="bad"></Button>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   );
 }
